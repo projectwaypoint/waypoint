a761510f57ad4795e43bd659e550f9138703c2b0
Jon Toy <jon@Jons-MacBook-Pro.local>
2018-04-20 12:59:00 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.use(express.static('public'));
+
+app.listen(3001, function () {
+  console.log('Waypoint listening on port 3001!');
+})
