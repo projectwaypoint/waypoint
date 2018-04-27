f011933cb45bf415922bf78206b9000fd0687f67
Nick.Randall <nick.randall@mcmurrytmg.com>
2018-04-26 23:38:03 -0700

    Update index location

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/build/index.html');
 });
 
 app.use(express.static('public'));
